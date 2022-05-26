c6acefafe623a16b6e62af1c9e59606595bc1eb1
gameowl <57566391+gameowl@users.noreply.github.com>
2022-05-26 19:18:54 +0300

    push

--- lab_1/js.js
+++ lab_1/js.js
@@ -15,3 +15,16 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+jQuery(document).ready(function() {
+    // pop-up close
+    jQuery(".corner_x").click(function() {
+       jQuery(this).parent(".sugestie").fadeOut();
+    });
+
+    //learn more
+    jQuery("#learnmore").click(function() {
+        jQuery(".learn-text").load("content.html");
+        return false;
+    });
+
+});
